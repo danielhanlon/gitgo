09b9f4a315c40c648d4a4ef39d04781dd4fad7b6
Daniel Hanlon <danielhanlon@gmail.com>
2017-04-10 17:06:10 +0100

    parsing flags

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
-import "fmt"
+import (
+  "fmt"
+  flag "github.com/ogier/pflag"
+)
+
+var (
+  user string
+)
+
 func main() {
-    fmt.Println("Hello, World")
+  fmt.Println("Hello, World")
+  flag.Parse()
+}
+
+//Run before main
+func init() {
+  //expecting a string, variable to bind to, flag to expect, default value, description
+  flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
